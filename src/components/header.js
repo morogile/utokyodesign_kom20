da2ad516a78de89beefe4b84022cf9a5efb56736
morogile <ama51.morota@gmail.com>
2020-11-10 10:44:12 +0900

    wawawa

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,55 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+import { Logo } from "./logo";
+
+const Container = styled.div`
+  height: 80px;
+  padding: 0 70px;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: solid 1px var(--color-gray);
+`;
+
+const Box = styled.div`
+  display: inline-block;
+  height: 100%;
+  padding: 0 20px;
+`;
+
+const NavContainer = styled.div`
+  display: inline-block;
+  padding: 0 20px;
+`;
+
+const NavItem = styled.h1`
+  font-size: 12px;
+  color: var(--color-blue);
+`;
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <Container>
+      <Logo />
+      <Box>
+        <NavContainer>
+          <Link to="/">
+            <NavItem>グラフィックデザイン概論とは</NavItem>
+          </Link>
+        </NavContainer>
+        <NavContainer>
+          <Link to="/lesson">
+            <NavItem>授業紹介</NavItem>
+          </Link>
+        </NavContainer>
+        <NavContainer>
+          <Link to="/gallery">
+            <NavItem>学生の作品ギャラリー</NavItem>
+          </Link>
+        </NavContainer>
+      </Box>
+    </Container>
   </header>
 )
 
