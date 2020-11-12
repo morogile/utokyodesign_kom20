09f5cc2b088a188672953f97ad2760e6db5b31f3
morogile <ama51.morota@gmail.com>
2020-11-12 15:44:37 +0900

    fix bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,10 @@ module.exports = {
     description: `グラフィックデザイン概論は、東京大学で開講されている講義です。受講生の成果発表や授業の紹介を行っています。`,
     author: `utokyo-design`,
   },
+  pathPrefix: "/reponame",
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -27,6 +29,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
